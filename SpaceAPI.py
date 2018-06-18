b08bbd71b5f49718ddc3207bfa9940face519b74
KrypticKoder <35192617+KrypticKoder@users.noreply.github.com>
2018-06-18 19:04:05 +1000

    Added files containing the code of ISS_Tracker

---
+++ SpaceAPI.py
@@ -0,0 +1,22 @@
+import requests
+import json
+import time
+
+#Get the response from the API endpoint.
+response = requests.get("http://api.open-notify.org/astros.json")
+data = response.json()
+
+print(data["number"])
+print(data)
+
+#ISS Location
+while 1 == 1:
+    time.sleep(1.0) #Delay so response doesn't overflow
+    response2 = requests.get("http://api.open-notify.org/iss-now.json") #ISS Information
+    data2 = response2.json()
+    print('Location update:')
+    print(data2) #Prints location of ISS
+
+
+
+
